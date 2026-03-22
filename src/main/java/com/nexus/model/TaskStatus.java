ec5c5c7637772a6114e9b49ca7371713709ab97f
nicejammer <158223817+nicejammer@users.noreply.github.com>
2026-03-22 23:17:58 +0000

    finish documenting the classes and some tweaks

--- src/main/java/com/nexus/model/TaskStatus.java
+++ src/main/java/com/nexus/model/TaskStatus.java
@@ -1,8 +1,29 @@
 package com.nexus.model;
 
 /**
- * Representa os estados possíveis de uma tarefa no motor Nexus.
+ * Enumeração que define os estados possíveis de uma {@link Task} no sistema Nexus.
+ * <ul>
+ *   <li><b>TO_DO</b> - Tarefa criada e pronta para ser iniciada</li>
+ *   <li><b>IN_PROGRESS</b> - Tarefa em andamento (requer um usuário atribuído)</li>
+ *   <li><b>BLOCKED</b> - Tarefa bloqueada, não pode progredir</li>
+ *   <li><b>DONE</b> - Tarefa concluída com sucesso</li>
+ * </ul>
  */
 public enum TaskStatus {
-    TO_DO, IN_PROGRESS, BLOCKED, DONE
+    /**
+     * Tarefa no estado inicial, pronta para ser iniciada.
+     */
+    TO_DO,
+    /**
+     * Tarefa em execução, atribuída a um usuário.
+     */
+    IN_PROGRESS,
+    /**
+     * Tarefa bloqueada e não pode fazer progressão.
+     */
+    BLOCKED,
+    /**
+     * Tarefa concluída com êxito.
+     */
+    DONE,
 }
